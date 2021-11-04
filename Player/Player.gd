3db564ec15c7ea6d1d98ff7e289966200e6e725b
Raf <rkrassowski@gmail.com>
2021-11-04 21:51:32 +0000

    move and sort
    basic y-sort and animation for some movement, moved player and bush to own scene

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,15 +1,28 @@
 extends KinematicBody2D
 
+const ACCELERATION = 700
+const MAX_SPEED = 80
+const FRICTION = 700
+
 var velocity = Vector2.ZERO
+
+onready var animationPlayer = $AnimationPlayer
  
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_raw_strength("ui_up")
+	input_vector = input_vector.normalized()
+	
 	
 	if input_vector != Vector2.ZERO:
-		velocity = input_vector
+		if input_vector.x > 0:
+			animationPlayer.play("RunRight")
+		else:
+			animationPlayer.play("RunLeft")
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
-		velocity = Vector2.ZERO
+		animationPlayer.play("IdleRight")
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 				
-	move_and_collide(velocity)
+	velocity = move_and_slide(velocity)
