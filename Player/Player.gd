6200f3cdbfb7e3db1a012e3e1c14be4930a322fd
Raf <rkrassowski@gmail.com>
2021-11-05 15:03:15 +0000

    Autotiling and good 8 direction animation
    Autotile full, 8 directional animation with tree

--- Player/Player.gd
+++ Player/Player.gd
@@ -7,22 +7,24 @@ const FRICTION = 700
 var velocity = Vector2.ZERO
 
 onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready	var animationState = animationTree.get("parameters/playback")
  
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_raw_strength("ui_up")
 	input_vector = input_vector.normalized()
-	
+
+	#states of animation saved in state machine if priority wrong increase length of y axis if you want left right
 	
 	if input_vector != Vector2.ZERO:
-		if input_vector.x > 0:
-			animationPlayer.play("RunRight")
-		else:
-			animationPlayer.play("RunLeft")
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationState.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
-		animationPlayer.play("IdleRight")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+		animationState.travel("Idle")
 				
 	velocity = move_and_slide(velocity)
