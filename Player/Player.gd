8960b7822ccf2543706ce556bcb51876db412747
Raf <rkrassowski@gmail.com>
2021-11-07 21:14:30 +0000

    now with evil grass and a bat
    hitboxes and hurtboxes also attach and roll

--- Player/Player.gd
+++ Player/Player.gd
@@ -2,29 +2,85 @@ extends KinematicBody2D
 
 const ACCELERATION = 700
 const MAX_SPEED = 80
+const ROLL_SPEED = 130
 const FRICTION = 700
 
-var velocity = Vector2.ZERO
+enum {
+	MOVE,
+	ROLL,
+	ATTACK
+}
 
+var state = MOVE
+
+var velocity = Vector2.ZERO
+var roll_vector = Vector2.LEFT
+ 
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready	var animationState = animationTree.get("parameters/playback")
+onready var swordCollisionShape = $HitboxPivot/SwordHitbox/CollisionShape2D
+onready var swordHitbox = $HitboxPivot/SwordHitbox
+
+func _ready():
+	animationTree.active = true
+	swordCollisionShape.disabled = true
+	swordHitbox.knockback_vector = roll_vector
  
 func _physics_process(delta):
+	match state:
+		MOVE:
+			move_state(delta)
+		ROLL:
+			roll_state(delta)
+		ATTACK:
+			attack_state(delta)
+	
+	
+func move_state(delta):
 	var input_vector = Vector2.ZERO
-	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_raw_strength("ui_up")
+	input_vector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	input_vector.y = Input.get_action_strength("move_down") - Input.get_action_raw_strength("move_up")
 	input_vector = input_vector.normalized()
 
 	#states of animation saved in state machine if priority wrong increase length of y axis if you want left right
 	
 	if input_vector != Vector2.ZERO:
+		roll_vector = input_vector
+		swordHitbox.knockback_vector = input_vector
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationTree.set("parameters/Attack/blend_position", input_vector)
+		animationTree.set("parameters/Roll/blend_position", input_vector)
 		animationState.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 		animationState.travel("Idle")
 				
+	move()
+	
+	if Input.is_action_just_pressed("roll"):
+		state = ROLL
+	
+	if Input.is_action_just_pressed("attack"):
+		state = ATTACK
+	
+func attack_state(delta):
+	velocity = Vector2.ZERO
+	animationState.travel("Attack")
+	
+func roll_state(delta):
+	velocity = roll_vector * ROLL_SPEED
+	animationState.travel("Roll")
+	move()
+	
+func move():
 	velocity = move_and_slide(velocity)
+	
+func attack_animation_finished():
+	state = MOVE
+	
+func roll_animation_finished():
+	state = MOVE
+	velocity = velocity /2
